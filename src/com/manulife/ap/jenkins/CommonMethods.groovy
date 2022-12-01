b3a9977161738984dfeab48361440e895c9a6f9b
Syd Pachica <Syd_Pachica@manulife.com>
2022-12-01 13:35:09 +0800

    Add line 6-10

--- src/com/manulife/ap/jenkins/CommonMethods.groovy
+++ src/com/manulife/ap/jenkins/CommonMethods.groovy
@@ -1,6 +1,13 @@
 package com.manulife.ap.jenkins
+import java.text.SimpleDateFormat
 
 public class CommonMethods {
+
+    static Script scriptObj
+
+    CommonMethods(Script scriptObj) {
+        this.scriptObj = scriptObj
+    }
 
     public String getVersionSuffix() {
         if (params.RC) {
